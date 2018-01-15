d8799e02e50fef9a6e188dc73c0afc138e7baed6
Linda Le <Linda@MacBook-Pro.attlocal.net>
2018-01-15 13:43:46 -0800

    second commit fixed errors

--- Backend/Controllers/PetsController.cs
+++ Backend/Controllers/PetsController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
-namespace spotalert.Controllers
+namespace Backend.Controllers
 {
     [Route("api/pets")]
-    public class PetController : Controller
+    public class PetsController : Controller
     {
        
     }
--- Backend/SpotAlertContext.cs
+++ Backend/SpotAlertContext.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Controllers;
 using Microsoft.EntityFrameworkCore;
-using User.Controllers;
-using Pet.Controllers;
 
 namespace spotalert
 {
