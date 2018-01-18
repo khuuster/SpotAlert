742476f66b2e6f7ffc6f6b25c016dd6d33a9a4c2
khuuster <khuu.andre@gmail.com>
2018-01-18 09:42:44 -0800

    nav bar moved to index

---
+++ Frontend/controllers/navBarController.js
@@ -0,0 +1,9 @@
+app.controller("navBarController", function ($scope, $state, $stateParams, $http, userService) {
+
+  $scope.userService = userService; 
+
+  $scope.loggingOut = function(){
+    userService.setCurrentUser(0);
+  }
+
+})
