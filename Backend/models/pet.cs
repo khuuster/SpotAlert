5b2a86b7374485982a23346904e9946d95b2e52f
Linda Le <Linda@MacBook-Pro.local>
2018-01-18 09:43:45 -0800

    pets images added to backend and frontend

--- Backend/models/pet.cs
+++ Backend/models/pet.cs
@@ -9,7 +9,7 @@ namespace Backend.Controllers
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string ImageUrl {get; set; }
+        public string Image {get; set; }
         public string LastKnownLoc {get; set;}
         public string Status {get; set; }
         public string LostSince {get; set;}   
