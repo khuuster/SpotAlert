6d825489da4f5838bb800b87c98f2fe8cbe7746f
Linda Le <Linda@MacBook-Pro.local>
2018-01-22 12:04:04 -0800

    all form validations updated

--- Frontend/controllers/petController.js
+++ Frontend/controllers/petController.js
@@ -7,6 +7,8 @@ app.controller("petController", function ($scope, $state, $stateParams, $http, p
   $scope.descriptionReq = false;
   $scope.imageReq = false;
   $scope.statusReq = false;
+  $scope.missingSinceReq = false;
+  $scope.lastKNownLocationReq = false;
 
   //ADDS A NEW PET FOR CURRENT USER
   $scope.addPet = function () {
@@ -38,6 +40,18 @@ app.controller("petController", function ($scope, $state, $stateParams, $http, p
     else {
       $scope.statusReq = false;
     };
+    if ($scope.missingSince == "" || $scope.missingSince == null) {
+      $scope.missingSinceReq = true;
+    }
+    else {
+      $scope.missingSinceReq = false;
+    };
+    if ($scope.lastKnownLocation == "" || $scope.lastKnownLocation == null) {
+      $scope.lastKnownLocationReq = true;
+    }
+    else {
+      $scope.lastKnownLocationReq = false;
+    };
 
     if ($scope.petName != "" && $scope.petName != null && $scope.description != "" && $scope.description != null && $scope.image != "" && $scope.image != null && $scope.status != "" && $scope.status != null) {
       petService.postPet(newPet);
@@ -55,15 +69,34 @@ app.controller("petController", function ($scope, $state, $stateParams, $http, p
     $state.go("lostUpdate");
   }
 
+  $scope.missingReq = false;
+  $scope.locationReq = false;
+
   //CHANGES PET STATUS TO LOST AND ADDS LAST KNOWN LOCATION
   $scope.report = function () {
     var petInfo = ({
       id: petService.returnLostPet().id, name: petService.returnLostPet().name, image: petService.returnLostPet().image, lastKnownLoc: $scope.lostLocation, status: "lost", lostSince: $scope.lostDate, description: petService.returnLostPet().description, ownerId: petService.returnLostPet().ownerId
     })
-    petService.updatePetLost(petInfo).then(function () {
-      petService.setCurrentPet(null);
-      $state.go("lostPets");
-    })
+
+    if ($scope.lostDate == "" || $scope.lostDate == null) {
+      $scope.missingReq = true;
+    }
+    else {
+      $scope.missingReq = false;
+    };
+    if ($scope.lostLocation == "" || $scope.lostLocation == null) {
+      $scope.locationReq = true;
+    }
+    else {
+      $scope.locationReq = false;
+    };
+
+    if ($scope.lostDate != "" && $scope.lostDate != null && $scope.lostLocation != "" && $scope.lostLocation != null) {
+      petService.updatePetLost(petInfo).then(function () {
+        // petService.setCurrentPet(null);
+        $state.go("lostPets");
+      })
+    }
   }
 
   // LOADS ALL PETS FOR CURRENT USER
@@ -141,10 +174,50 @@ app.controller("petController", function ($scope, $state, $stateParams, $http, p
       id: petService.returnCurrentPet().id,
       Name: $scope.petName, Image: $scope.image, Description: $scope.description, LostSince: $scope.missingSince, LastKnownLoc: $scope.lastKnownLocation, Status: $scope.status, OwnerId: userService.currentUserReturn()
     })
-    petService.updatePet(updatePet).then(function () {
-      petService.setCurrentPet(null);
-      $state.go("pets");
-    })
+
+    if ($scope.petName == "" || $scope.petName == null) {
+      $scope.petNameReq = true;
+    }
+    else {
+      $scope.petNameReq = false;
+    };
+    if ($scope.description == "" || $scope.description == null) {
+      $scope.descriptionReq = true;
+    }
+    else {
+      $scope.descriptionReq = false;
+    };
+    if ($scope.image == "" || $scope.image == null) {
+      $scope.imageReq = true;
+    }
+    else {
+      $scope.imageReq = false;
+    };
+    if ($scope.status == "" || $scope.status == null) {
+      $scope.statusReq = true;
+    }
+    else {
+      $scope.statusReq = false;
+    };
+    if ($scope.missingSince == "" || $scope.missingSince == null) {
+      $scope.missingSinceReq = true;
+    }
+    else {
+      $scope.missingSinceReq = false;
+    };
+    if ($scope.lastKnownLocation == "" || $scope.lastKnownLocation == null) {
+      $scope.lastKnownLocationReq = true;
+    }
+    else {
+      $scope.lastKnownLocationReq = false;
+    };
+
+    if ($scope.petName != "" && $scope.petName != null && $scope.description != "" && $scope.description != null && $scope.image != "" && $scope.image != null && $scope.status != "" && $scope.status != null) {
+      petService.updatePet(updatePet).then(function () {
+        // petService.setCurrentPet(null);
+        $state.go("pets");
+      })
+    }
   }
 
   //DELETES PET
@@ -154,6 +227,7 @@ app.controller("petController", function ($scope, $state, $stateParams, $http, p
       $state.go("pets");
     })
   }
+
   $scope.messageDateReq = false;
   $scope.messageNameReq = false;
   $scope.messageNumberReq = false;
@@ -166,12 +240,45 @@ app.controller("petController", function ($scope, $state, $stateParams, $http, p
       id: $scope.owner.id, firstName: $scope.owner.firstName, lastName: $scope.owner.lastName, email: $scope.owner.email, password: $scope.owner.password, phoneNumber: $scope.owner.phoneNumber, address: $scope.owner.address, messageDate: $scope.messageDate, messageName: $scope.messageName, messageNumber: $scope.messageNumber, messageEmail: $scope.messageEmail, message: $scope.message
     })
 
-    userService.setOwner($scope.owner.id);
-    userService.messageOwner(owner).then(function(){
-      petService.setCurrentPet(null); {
-        $state.go("lostPets");
-      }
-    })
+    if ($scope.messageDate == "" || $scope.messageDate == null) {
+      $scope.messageDateReq = true;
+    }
+    else {
+      $scope.messageDateReq = false;
+    };
+    if ($scope.messageName == "" || $scope.messageName == null) {
+      $scope.messageNameReq = true;
+    }
+    else {
+      $scope.messageNameReq = false;
+    };
+    if ($scope.messageNumber == "" || $scope.messageNumber == null) {
+      $scope.messageNumberReq = true;
+    }
+    else {
+      $scope.messageNumberReq = false;
+    };
+    if ($scope.messageEmail == "" || $scope.messageEmail == null) {
+      $scope.messageEmailReq = true;
+    }
+    else {
+      $scope.messageEmailReq = false;
+    };
+    if ($scope.message == "" || $scope.message == null) {
+      $scope.messageReq = true;
+    }
+    else {
+      $scope.messageReq = false;
+    };
+
+    if ($scope.messageDate != "" && $scope.messageDate != null && $scope.messageName != "" && $scope.messageName != null && $scope.messageNumber != "" && $scope.messageNumber != null && $scope.messageEmail != "" && $scope.messageEmail != null && $scope.message != "" && $scope.message != null) {
+      userService.setOwner($scope.owner.id);
+      userService.messageOwner(owner).then(function(){
+        petService.setCurrentPet(null); {
+          $state.go("lostPets");
+        }
+      })
+    }
   }
 
   //GOOGLE API MAP FUNCTION
